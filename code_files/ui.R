e289a03086152ebbfdecc52598948099e6416379
Steve Pfeiffer (EPA-ORD) <Pfeiffer.Steven@epa.gov>
2022-03-02 15:19:26 -0500

    add Google Tag Manager code

--- code_files/ui.R
+++ code_files/ui.R
@@ -6,11 +6,26 @@ library(shinydashboard)
 library(tidyr)
 
 ui <- fluidPage(
-				#useShinyjs(),
-                tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "style.css")),
+                tags$head(
+							HTML("
+								<!-- Google Tag Manager -->
+								<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+								new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+								j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+								'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+								})(window,document,'script','dataLayer','GTM-L8ZB');</script>
+								<!-- End Google Tag Manager -->
+							    "
+							   ),
+							tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+						 ),
                 tags$body(class="html wide-template"),
                 fluidRow(
                 				HTML	("
+											<!-- Google Tag Manager (noscript) -->
+											<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-L8ZB'
+											height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
+											<!-- End Google Tag Manager (noscript) -->
                 						<header class='masthead clearfix' role='banner'>
                 							<img alt='' class='site-logo' src='https://www.epa.gov/sites/all/themes/epa/logo.png'>
                 							<div class='site-name-and-slogan'>
